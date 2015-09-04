b26bd504f586cf7741447b1bad8381a9d0c87e23
Alexandre Sadowski <asadowski@beapi.fr>
2015-09-04 16:17:29 +0200

    fix wrong param for locate_template

--- classes/helpers.php
+++ classes/helpers.php
@@ -30,7 +30,7 @@ class Helpers {
 		$path = apply_filters( 'BEA/Helpers/locate_template/templates', array( 'views/' . BEA_PB_VIEWS_FOLDER_NAME . '/' . $tpl . '.php' ), $tpl, __NAMESPACE__ );
 
 		// Locate from the theme
-		$located = locate_template( array( 'views/' . BEA_PB_VIEWS_FOLDER_NAME . '/' . $tpl . '.php' ), false, false );
+		$located = locate_template( $path, false, false );
 		if ( ! empty( $located ) ) {
 			return $located;
 		}
